3b7eaf5fcc38ea857b9634d9bdfffb50bd6f6d8c
ivolianer <ivolianer2014@163.com>
2017-10-21 23:30:07 +0800

    更新常用依赖至最新版本

--- app/src/main/java/com/ivotai/lean/UserAct.kt
+++ app/src/main/java/com/ivotai/lean/UserAct.kt
@@ -1,10 +1,9 @@
 package com.ivotai.lean
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.ivotai.lean.user.User
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class UserAct : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
