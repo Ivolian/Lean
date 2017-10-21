811703b5c2db0b3a0375f345779984e0030f53da
ivolianer <ivolianer2014@163.com>
2017-10-21 18:30:12 +0800

    Initial commit

---
+++ app/src/main/java/com/ivotai/lean/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ivotai.lean
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ivotai.lean.user.User
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
---
+++ app/src/main/java/com/ivotai/lean/user/User.kt
@@ -0,0 +1,4 @@
+package com.ivotai.lean.user
+
+
+class User(val id: String, val name: String, val avatar: String)
